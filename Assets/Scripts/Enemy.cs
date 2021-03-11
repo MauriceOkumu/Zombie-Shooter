fc62c3b7acdac5b381125957880cea751d50eab8
maurice <maurice.okumu@thoughtworks.com>
2021-03-11 15:59:46 -0600

    Add enemy placeholder and AI | Maurice

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+   [SerializeField] Transform player;
+    NavMeshAgent enemy;
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(player.position);
+    }
+}
